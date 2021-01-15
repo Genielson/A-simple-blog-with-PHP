92d4fc622d8ff6ad079d34665d908047c5a5e39e
Genielson <30542509+Genielson@users.noreply.github.com>
2021-01-14 23:23:28 -0300

    Adicionando classe User

---
+++ source/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Source\Controllers\Models;
+
+class User {
+
+    private $nome;
+    private $sobrenome;
+    private $login;
+    private $password;
+
+    private function createAComent(){
+
+    }
+
+    private function authenticateUser(){
+        
+    }
+
+
+
+}
+
+
+?>
